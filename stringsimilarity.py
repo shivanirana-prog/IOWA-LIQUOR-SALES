8f57bee8d2eefad1ddd28c09e623532b46a2c95a
Saptarshi3670 <55178820+Saptarshi3670@users.noreply.github.com>
2020-03-25 03:49:55 -0500

    Partial validation of counties and cities based on longitude and latitude

--- stringsimilarity.py
+++ stringsimilarity.py
@@ -14,7 +14,15 @@ import pandas as pd
 
 import Levenshtein as lvnstn
 
-df = pd.read_csv('/Users/saptarshimaiti/Desktop/Data Preparation And Analysis/Project/Iowa_Liquor_Sales_Cleaned_v1.csv')
+import re
+
+import reverse_geocoder as rg 
+
+import gc
+
+
+
+df = pd.read_csv('/Users/saptarshimaiti/Desktop/Data Preparation And Analysis/Project/Iowa_Liquor_Sales_Cleaned_v2.csv', low_memory = False)
 
 #report = pp.ProfileReport(df)
 
@@ -29,6 +37,105 @@ cities = df['City'].unique()
 counties = df['County'].unique()
 
 
+cities_dict = {}
+cities_dict['Primary City'] = []
+cities_dict['Secondary City'] = []
+cities_dict['Similarity'] = []
 
 
 
+def citySimilarity(cities):
+    for city in cities:
+    
+        for city_sim in cities:
+            similarity = lvnstn.distance(city, city_sim)
+            cities_dict['Primary City'].append(city)
+            cities_dict['Secondary City'].append(city_sim)
+            cities_dict['Similarity'].append(similarity)
+
+citySimilarity(cities)    
+
+        
+df_similarity = pd.DataFrame.from_dict(cities_dict, orient='index').T
+
+df_similarity = df_similarity.drop_duplicates()
+
+df_similarity[df_similarity['Similarity'] == 2]
+
+        
+coordinates = df['Store Location'].unique()
+
+
+for coordinate in coordinates:
+    
+    longitude, latitude = re.sub(r' ',",",re.sub(r'\)',"",re.sub(r'POINT \(',"", coordinate))).split(',')
+    city = rg.search((latitude, longitude))[0]['name']
+    df.loc[(df['City'] != city) & (df['Store Location'] == coordinate), 'City'] = city
+    
+    
+
+ 
+cities = df['City'].unique()
+
+counties = df['County'].unique()
+
+
+cities_dict = {}
+cities_dict['Primary City'] = []
+cities_dict['Secondary City'] = []
+cities_dict['Similarity'] = []
+
+
+
+def citySimilarity(cities):
+    for city in cities:
+    
+        for city_sim in cities:
+            similarity = lvnstn.distance(city, city_sim)
+            cities_dict['Primary City'].append(city)
+            cities_dict['Secondary City'].append(city_sim)
+            cities_dict['Similarity'].append(similarity)
+
+citySimilarity(cities)    
+
+df_similarity_tst = pd.DataFrame.from_dict(cities_dict, orient='index').T
+
+df_similarity_tst = df_similarity_tst.drop_duplicates()
+
+df_similarity_tst[df_similarity_tst['Similarity'] == 1]
+
+#-------------- County ----------#
+
+county_dict = {}
+county_dict['Primary County'] = []
+county_dict['Secondary County'] = []
+county_dict['Similarity'] = []
+
+
+
+def countySimilarity(counties):
+    for county in counties:
+        for county_sim in counties:
+            similarity = lvnstn.distance(county, county_sim)
+            county_dict['Primary County'].append(county)
+            county_dict['Secondary County'].append(county_sim)
+            county_dict['Similarity'].append(similarity)
+
+countySimilarity(counties) 
+
+df_similarity = pd.DataFrame.from_dict(county_dict, orient='index').T
+
+df_similarity = df_similarity.drop_duplicates()
+
+df_similarity[df_similarity['Similarity'] == 1]
+
+
+
+for coordinate in coordinates:
+    
+    longitude, latitude = re.sub(r' ',",",re.sub(r'\)',"",re.sub(r'POINT \(',"", coordinate))).split(',')
+    county = re.sub(r' County',"",rg.search((latitude, longitude))[0]['admin2'])
+    df.loc[(df['County'] != county) & (df['Store Location'] == coordinate), 'County'] = county
+    
+df.to_csv(r'/Users/saptarshimaiti/Desktop/Data Preparation And Analysis/Project/Iowa_Liquor_Sales_Cleaned_v3.csv', index = False, header=True)
+gc.collect()
